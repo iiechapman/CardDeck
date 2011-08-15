d6120fa1f9abc684fbae9bb12944c6602bc90439
Evan Chapman <iiechapman@gmail.com>
2011-08-15 12:50:57 -0400

    Cleaned up
    Clean up 1

--- src/com/vindince/test/CardDeck.java
+++ src/com/vindince/test/CardDeck.java
@@ -3,9 +3,6 @@ package com.vindince.test;
 public class CardDeck {
 	
 	
-	System.out.println("This works?");
-
-	
 	public static void main (String args[]){
 		
 		System.out.println("Finally JEEZ");
